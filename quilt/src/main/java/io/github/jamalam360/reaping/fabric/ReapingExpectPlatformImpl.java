22b3b7ef0a429875f583a8831eaa4902a2ac6735
Jamalam360 <56727311+Jamalam360@users.noreply.github.com>
2022-06-10 23:04:52 +0100

    fix: fix comp errors

--- quilt/src/main/java/io/github/jamalam360/reaping/fabric/ReapingExpectPlatformImpl.java
+++ quilt/src/main/java/io/github/jamalam360/reaping/fabric/ReapingExpectPlatformImpl.java
@@ -1,10 +1,6 @@
 package io.github.jamalam360.reaping.fabric;
 
-import io.github.jamalam360.reaping.fabriclike.mixin.LootContextBuilderAccessor;
 import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.damage.DamageSource;
-import net.minecraft.loot.context.LootContext;
-import org.quiltmc.loader.api.QuiltLoader;
 import virtuoel.pehkui.api.ScaleData;
 import virtuoel.pehkui.api.ScaleTypes;
 
@@ -12,14 +8,6 @@ import virtuoel.pehkui.api.ScaleTypes;
  * @author Jamalam360
  */
 public class ReapingExpectPlatformImpl {
-    public static boolean isModLoaded(String modId) {
-        return QuiltLoader.isModLoaded(modId);
-    }
-
-    public static LootContext.Builder getLootContextBuilder(LivingEntity entity, boolean causedByPlayer, DamageSource source) {
-        return ((LootContextBuilderAccessor) entity).callGetLootContextBuilder(causedByPlayer, source);
-    }
-
     public static void setScale(LivingEntity entity, float scale) {
         ScaleData data = ScaleTypes.BASE.getScaleData(entity);
         data.setScale(scale);
