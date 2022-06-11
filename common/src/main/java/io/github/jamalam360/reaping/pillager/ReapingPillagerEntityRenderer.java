ef5589c73892815860cbf32640c4ba7a150b6899
Jamalam360 <56727311+Jamalam360@users.noreply.github.com>
2022-06-11 14:51:13 +0100

    wip: pillager renderer

---
+++ common/src/main/java/io/github/jamalam360/reaping/pillager/ReapingPillagerEntityRenderer.java
@@ -0,0 +1,24 @@
+package io.github.jamalam360.reaping.pillager;
+
+import net.minecraft.client.render.entity.EntityRendererFactory;
+import net.minecraft.client.render.entity.IllagerEntityRenderer;
+import net.minecraft.client.render.entity.feature.HeldItemFeatureRenderer;
+import net.minecraft.client.render.entity.model.EntityModelLayers;
+import net.minecraft.client.render.entity.model.IllagerEntityModel;
+import net.minecraft.util.Identifier;
+
+/**
+ * @author Jamalam
+ */
+public class ReapingPillagerEntityRenderer extends IllagerEntityRenderer<ReapingPillagerEntity> {
+    private static final Identifier TEXTURE = new Identifier("textures/entity/illager/pillager.png");
+
+    public ReapingPillagerEntityRenderer(EntityRendererFactory.Context context) {
+        super(context, new IllagerEntityModel<>(context.getPart(EntityModelLayers.PILLAGER)), 0.5F);
+        this.addFeature(new HeldItemFeatureRenderer<>(this, context.getHeldItemRenderer()));
+    }
+
+    public Identifier getTexture(ReapingPillagerEntity entity) {
+        return TEXTURE;
+    }
+}
