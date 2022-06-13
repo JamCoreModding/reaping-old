fc263cf15b0e296ad92e2e811797ef41acadd463
Jamalam360 <56727311+Jamalam360@users.noreply.github.com>
2022-06-13 14:10:22 +0100

    feat: more pillager stuff

--- fabric-like/src/main/java/io/github/jamalam360/reaping/fabriclike/mixin/pillager/ChunkGeneratorMixin.java
+++
@@ -1,5 +0,0 @@
-package io.github.jamalam360.reaping.fabriclike.mixin.pillager;/**
- * @author  Jamalam
- */
-public class ChunkGeneratorMixin {
-}
--- forge/src/main/java/io/github/jamalam360/reaping/forge/ReapingForge.java
+++ forge/src/main/java/io/github/jamalam360/reaping/forge/ReapingForge.java
@@ -3,8 +3,14 @@ package io.github.jamalam360.reaping.forge;
 import dev.architectury.platform.forge.EventBuses;
 import io.github.jamalam360.reaping.ReapingMod;
 import io.github.jamalam360.reaping.config.ReapingConfig;
+import io.github.jamalam360.reaping.registry.ReapingEntities;
 import me.shedaniel.autoconfig.AutoConfig;
+import net.minecraft.entity.SpawnGroup;
+import net.minecraft.world.biome.SpawnSettings;
+import net.minecraft.world.gen.feature.StructureFeatures;
 import net.minecraftforge.client.ConfigGuiHandler;
+import net.minecraftforge.event.world.StructureSpawnListGatherEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
@@ -19,5 +25,12 @@ public class ReapingForge {
                 ConfigGuiHandler.ConfigGuiFactory.class,
                 () -> new ConfigGuiHandler.ConfigGuiFactory((minecraftClient, screen) -> AutoConfig.getConfigScreen(ReapingConfig.class, screen).get())
         );
+    }
+
+    @SubscribeEvent
+    public void onStructureSpawnListGatherEvent(StructureSpawnListGatherEvent event) {
+        if (event.getStructure() == StructureFeatures.PILLAGER_OUTPOST.get()) {
+            event.addEntitySpawn(SpawnGroup.MONSTER, new SpawnSettings.SpawnEntry(ReapingEntities.REAPING_PILLAGER.get(), 1, 1, 1));
+        }
     }
 }
